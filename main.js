b95c4c1d0443a2f56b4ca92c4ce14f512a6652ed
anjalichandwani04 <122337018+anjalichandwani04@users.noreply.github.com>
2023-07-30 02:19:09 +0530

    Uploaded my code for blog website

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+function closeclick (){
+    console.log("Button clicked close");
+}
+
+function loginclicked (){
+    login-popup.classList.add("show");
+}
+
+loginbutt.addEventListener("click", loginclicked)
